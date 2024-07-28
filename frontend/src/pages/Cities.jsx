d89a3facca2e705e9b7575aeb96de0f1b81f5309
prerna351 <prernasharma7008@gmail.com>
2024-07-29 00:34:08 +0530

    modified heading styling

--- frontend/src/pages/Cities.jsx
+++ frontend/src/pages/Cities.jsx
@@ -16,7 +16,7 @@ export const Cities = () => {
         <div className='w-full h-screen px-6 md:px-10 flex justify-center'>
             <div className='mt-20 w-full max-w-[1100px] h-screen'>
 
-               <div className='flex justify-center font-bold text-5xl pb-20'><h1>View Rooms in <span className='text-green-500 ml-1'>Popular Cities</span></h1></div>
+               <div className='flex justify-center font-bold text-3xl md:text-5xl pb-20'><h1>View Rooms in <span className='text-green-500 ml-1'>Popular Cities</span></h1></div>
                <div className='w-full grid grid-cols-3 gap-7 sm:grid-cols-4 md:gap-9 lg:grid-cols-5'>
                 <CityCard image_link={"/cities/delhi.jpg"} city_name={"Delhi"} />
                 <CityCard image_link={"/cities/mumbai.jpg"} city_name={"Mumbai"}/>
