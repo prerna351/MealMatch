c8706a8e1b6cdd56128c405da5dfae0445455a3a
prerna351 <prernasharma7008@gmail.com>
2024-07-30 21:30:54 +0530

    added forms state

---
+++ frontend/src/store/atoms/FormsState.js
@@ -0,0 +1,30 @@
+import { atom } from 'recoil';
+
+export const ownerDetailsAtom = atom({
+  key: 'ownerDetailsAtom', // unique ID for this atom
+  default: {
+    ownerName: '',
+    contactNumber: '',
+    streetAddress: '',
+    pincode: '',
+    city: '',
+    state: '',
+    country: "India"
+    
+  },
+});
+
+import { atom } from 'recoil';
+
+export const mealDetailsAtom = atom({
+  key: 'mealDetailsAtom', // unique ID for this atom
+  default: {
+    mealTypes: [], // e.g., ['Breakfast', 'Lunch', 'Dinner']
+    pricing: {
+      breakfast: '',
+      lunch: '',
+      dinner: '',
+    },
+    // Add other fields as necessary
+  },
+});
